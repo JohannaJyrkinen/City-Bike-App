fa0d24771bf390da34618251fcb458beb0999776
Johanna Jyrkinen <johanna.jyrkinen@outlook.com>
2023-05-15 10:37:09 +0300

    Update journey card

--- client/src/components/Journey.js
+++ client/src/components/Journey.js
@@ -1,11 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import { Card } from 'react-bootstrap'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import React from 'react'
 
-export function Journey() {
+export function Journey({journey}) {
+
+    const distanceKm = journey.distance / 1000
+    const roundedNumber = distanceKm.toFixed(1)
+
+    const journeyInMin = journey.duration/60
+    const fullMinutes = Math.round(journeyInMin)
+
     return (
-        <Card>
-            <Card.Body style={{textAlign: "center", fontFamily: 'Gotham Rounded, sans-serif'}}> Startpoint  Endpoint    distance    duration</Card.Body>
+        <Card style= {{marginBottom: "10px"}}>
+            <Card.Body style={{textAlign: "center", fontFamily: 'Gotham Rounded, sans-serif'}}>
+                    <Row>
+                    <Col>{journey.departureStation}</Col>
+                    <Col>{journey.returnStation} </Col> 
+                    <Col>{roundedNumber} km</Col>
+                    <Col> {fullMinutes} min</Col>
+                    </Row>
+            </Card.Body>
         </Card>
     )
 }
--- client/src/pages/Journeys.js
+++ client/src/pages/Journeys.js
@@ -1,19 +1,69 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, {useEffect, useState}  from "react";
+import axios from "axios";
+import { Container, Button } from "react-bootstrap";
 import { Journey } from "../components/Journey";
 import NavBar from "../components/NavBar";
 import "./journeys.css"
 
 export function Journeys() {
 
+    const [journeys, setJourneys] = useState(null)
+    const [currentPage, setCurrentPage] = useState(1)
+
+    useEffect(() => {
+        getJourneys(currentPage);
+    }, [currentPage]);
+
+    const getJourneys = async (pageNumber) => {
+        try {
+          const response = await axios.get(`http://localhost:3001/api/journeys?pageNumber=${pageNumber}`);
+          const data = response.data;
+          setJourneys(data.journeys.data);
+          console.log(data.journeys.data);
+        } catch (error) {
+          console.log(error);
+        }
+    };
+
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+          setCurrentPage(currentPage - 1);
+          scrollToTop();
+        }
+    };
+    
+    const handleNextPage = () => {
+        setCurrentPage(currentPage + 1);
+        scrollToTop(); 
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
+
     return(
         <div>
             <NavBar />
             <Container>
                 <header>
-                    <h1 style = {{fontFamily: 'Gotham Rounded, sans-serif', marginTop: "65px", textAlign: 'center' }}>Matkat</h1>
+                    <h1 style = {{fontFamily: 'Gotham Rounded, sans-serif', marginTop: "65px", textAlign: 'center' }}>Journeys</h1>
                 </header>
-                <Journey/>
+                <Container >
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                        {currentPage > 1 && (<Button variant="dark" className="pagination-button" onClick={handlePreviousPage} style={{ fontFamily: 'Gotham Rounded, sans-serif', marginRight: "20px" }}>Previous</Button>)}
+                        <Button variant="dark" className="pagination-button" onClick={handleNextPage}style={{ fontFamily: 'Gotham Rounded, sans-serif'}} >Next</Button>
+                    </div>
+                </Container>
+                <br/>
+                {journeys ? (journeys.map((journey)=> <Journey key={journey._id} journey={journey}/>)):(<> </>)}
+                <Container >
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                        {currentPage > 1 && (<Button variant="dark" className="pagination-button" onClick={handlePreviousPage} style={{ fontFamily: 'Gotham Rounded, sans-serif', marginRight: "20px" }}>Previous</Button>)}
+                        <Button variant="dark" className="pagination-button" onClick={handleNextPage}style={{ fontFamily: 'Gotham Rounded, sans-serif'}} >Next</Button>
+                    </div>
+                </Container>
+                <br/>
             </Container>
         </div>
     )
