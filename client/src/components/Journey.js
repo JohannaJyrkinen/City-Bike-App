49b0e62135604eb0b9f107bd0d197eddf4a70207
Johanna Jyrkinen <johanna.jyrkinen@outlook.com>
2023-05-15 12:18:02 +0300

    Fix appearance in small screens, add css

--- client/src/components/Journey.js
+++ client/src/components/Journey.js
@@ -3,25 +3,30 @@ import { Card } from 'react-bootstrap'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import React from 'react'
+import '../css/journey.css'
 
 export function Journey({journey}) {
 
     const distanceKm = journey.distance / 1000
-    const roundedNumber = distanceKm.toFixed(1)
+    const roundedNumber = distanceKm.toFixed(2)
 
     const journeyInMin = journey.duration/60
     const fullMinutes = Math.round(journeyInMin)
 
     return (
         <Card style= {{marginBottom: "10px"}}>
-            <Card.Body style={{textAlign: "center", fontFamily: 'Gotham Rounded, sans-serif'}}>
+            <Card.Body className='custom-card-body'style={{textAlign: "center", fontFamily: 'Gotham Rounded, sans-serif'}}>
                     <Row>
                     <Col>{journey.departureStation}</Col>
                     <Col>{journey.returnStation} </Col> 
-                    <Col>{roundedNumber} km</Col>
-                    <Col> {fullMinutes} min</Col>
+                    <Col xs={2}>{roundedNumber} km</Col>
+                    <Col xs={2}> {fullMinutes} min</Col>
                     </Row>
             </Card.Body>
+
+
         </Card>
+
+
     )
 }
