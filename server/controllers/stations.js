1cb5c68a87b18618abed0dd79031b9d67c76fc6c
Johanna Jyrkinen <johanna.jyrkinen@outlook.com>
2023-05-19 09:29:16 +0300

    Add endpoint for adding new stations

--- server/controllers/stations.js
+++ server/controllers/stations.js
@@ -5,29 +5,51 @@ stationsRouter.get('/', async (req, res) => {
   try {
     const pageNumber = parseInt(req.query.pageNumber, 10) || 0
     const limit = parseInt(req.query.limit, 10) || 25
+    const query = req.query.query || ''
+
     const results = {}
-    const totalStations = await Station.countDocuments().exec()
-    const startIndex = (pageNumber - 1) * limit
+    let totalStations
+    let stationsQuery = {}
+
+    if (query) {
+      stationsQuery.Nimi = { $regex: new RegExp(query, 'i') } 
+      totalStations = await Station.countDocuments(stationsQuery).exec()
+    } else {
+      totalStations = await Station.countDocuments().exec()
+    }
+
+    const startIndex = Math.max((pageNumber - 1) * limit, 0)
     const endIndex = pageNumber * limit
       
     if (startIndex > 0) {
       results.previous = {
         pageNumber: pageNumber -1,
         limit: limit, 
+        query: query,
       }
     }
     if (endIndex < totalStations) {
       results.next = {
         pageNumber: pageNumber + 1,
         limit: limit,
+        query: query,
 
       }
     }
-    results.data = await Station.find({})
-      .sort({})
-      .limit(limit)
-      .skip(startIndex)
-      .exec()
+
+    if (query) {
+      results.data = await Station.find(stationsQuery)
+        .sort({})
+        .limit(limit)
+        .skip(startIndex)
+        .exec()
+    } else {
+      results.data = await Station.find({})
+        .sort({})
+        .limit(limit)
+        .skip(startIndex)
+        .exec()
+    }  
     results.rows = limit
     return res.json({stations: results})
   } catch (error) {
@@ -35,6 +57,22 @@ stationsRouter.get('/', async (req, res) => {
     return res.status(500).json({ msg: error })
   }
     
+})
+
+stationsRouter.post('/', async (req, res) => {
+  try {
+    const stationData = req.body
+    if (!stationData.ID || !stationData.Nimi || !stationData.Osoite || !stationData.Kaupunki || !stationData.x || !stationData.y) {
+      return res.status(400).json({ msg: 'Required fields are missing' })
+    }
+    const newStation = new Station(stationData)
+    const savedStation = await newStation.save()
+    return res.status(201).json(savedStation)
+
+  } catch (error){
+    console.log(error)
+    return res.status(500).json({ msg: error })
+  } 
 })
 
 module.exports = stationsRouter 
